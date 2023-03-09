04f81870a87e2c8bc188d7f1a6ecbf7492edf548
flgaitan <fiama_gaitan92@hotmail.com>
2023-03-09 08:56:25 -0600

    working on heroku deployment

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { application } = require('express');
+const { app } = require('express');
 const apiRoutes = require('./api');
 
 router.use('/',require('./homeRoutes'));
