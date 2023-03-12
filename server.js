c6ce6ebbd271c6929bc63e711423b6b4e6ab3814
flgaitan <fiama_gaitan92@hotmail.com>
2023-03-12 11:57:53 -0500

    changes in index.js

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+//const routes = require('./controllers');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+//const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 // Set up Handlebars.js 
 //const hbs = exphbs.create({});
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//  //   db: sequelize
+//   })
+// };
 
-app.use(session(sess));
+//app.use(session(sess));
+
+const article = require('./models/Article');
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
@@ -33,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 //routes are in the controllers but no folder named routes
-app.use(routes);
+//app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Now listening on http://localhost:${PORT}`);
